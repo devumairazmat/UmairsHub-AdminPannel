9b52c827c7bbe2ffff83244e5c96b4dfb138d4dd
Umair Azmat <umairazmat8260@gmail.com>
2022-05-26 02:01:09 +0500

    User Added Firebase

--- src/components/sales/Sales.js
+++ src/components/sales/Sales.js
@@ -1,153 +1,166 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import {db} from "../../config/Config"
-import {collection, doc, getDoc, getDocs, addDoc } from "firebase/firestore";
+import { db } from "../../config/Config";
+import { collection, doc, getDoc, getDocs, addDoc } from "firebase/firestore";
 
 export default function Sales() {
-    const [salesProducts, setSalesProducts] = useState([])
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
-    const [price, setPrice] = useState("")
-    const [imgLink, setImgLink] = useState("")
-    const [sale, setSale] = useState("")
-    const [showSale, setShowSale] = useState(false)
-    const salesUpdateData = async() => {
-      let salesAllData=[]
-        const querySnapshot = await getDocs(collection(db, "sales"));
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-          let data = {...doc.data(), id: doc.id};
-          salesAllData.push(data);
-        });
-        setSalesProducts(salesAllData)
-        setShowSale(true)
-        console.log("user data sales",salesAllData)
-      }
-    useEffect(
-      () => {
-        salesUpdateData()
-      },[]
-    )
+  const [salesProducts, setSalesProducts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [imgLink, setImgLink] = useState("");
+  const [sale, setSale] = useState("");
+  const [showSale, setShowSale] = useState(false);
+  const salesUpdateData = async () => {
+    let salesAllData = [];
+    const querySnapshot = await getDocs(collection(db, "sales"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      let data = { ...doc.data(), id: doc.id };
+      salesAllData.push(data);
+    });
+    setSalesProducts(salesAllData);
+    setShowSale(true);
+    console.log("user data sales", salesAllData);
+  };
+  useEffect(() => {
+    salesUpdateData();
+  }, []);
   return (
     <div>
-            <h1>Sales</h1>
-        <div className="addSales">
-            <div className="addSales-form">
-                <div className="addSales-form-title">
-                    <h1>Add Sales</h1>
-                </div>
-                <div className="addSales-form-input">
-                    <label htmlFor="title" >Title</label>
-                    <input type="text" placeholder="Title" id="title" onChange={
-                        (e) => {
-                            setTitle(e.target.value)
-                        }
-                    }/>
-                </div>
-                <div className="addSales-form-input">
-                    <label htmlFor="desc">Description</label>
-                    <input type="text" placeholder="Description" id="desc" onChange={
-                        (e) => {
-                            setDescription(e.target.value)
-                        }
-
-                    } />
-                </div>
-                <div className="addSales-form-input">
-                    <label htmlFor="price">Price</label>
-                    <input type="text" placeholder="Price" id="price" onChange={
-                        (e) => {
-                            setPrice(e.target.value)
-                        }
-                    } />
-                </div>
-                <div className="addSales-form-input">
-                    <label htmlFor="image">Image</label>
-                    <input type="text" placeholder="Image" id="image" onChange={
-                        (e) => {
-                            setImgLink(e.target.value)
-                        }
-                    }/>
-                </div>
-                <div className="addSales-form-input">
-                    <label htmlFor="Sale">Sale Price</label>
-                    <input type="text" placeholder="Sale Price" id="Sale" onChange={
-                        (e) => {
-                            setSale(e.target.value)
-                        }
-                    } /> 
-                </div>    
-
-
-            </div>      
-            <div className="addSales-form-button">
-                <button onClick={
-                   async () => {
-
-            try {
-            const docRef = await addDoc(collection(db, "sales"), {
-                Title:title,
-                Description :description,
-                price:price,
-                imgLink:imgLink,
-                sale:sale
-            });
-            console.log("Document written with ID: ", docRef.id);
-            } catch (e) {
-            console.error("Error adding document: ", e);
-            }
-            salesUpdateData()
-            setTitle("")
-            setDescription("")
-            setPrice("")
-            setImgLink("")
-            setSale("")
-            }
-                }>Add</button>
-                </div>
-
-
- 
+      <h1>Sales</h1>
+      <div className="addSales">
+        <div className="addSales-form">
+          <div className="addSales-form-title">
+            <h1>Sales Options</h1>
+          </div>
+          <div className="addSales-form-input">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              placeholder="Title"
+              id="title"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </div>
+          <div className="addSales-form-input">
+            <label htmlFor="desc">Description</label>
+            <input
+              type="text"
+              placeholder="Description"
+              id="desc"
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </div>
+          <div className="addSales-form-input">
+            <label htmlFor="price">Price</label>
+            <input
+              type="text"
+              placeholder="Price"
+              id="price"
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
+          </div>
+          <div className="addSales-form-input">
+            <label htmlFor="image">Image</label>
+            <input
+              type="text"
+              placeholder="Image"
+              id="image"
+              onChange={(e) => {
+                setImgLink(e.target.value);
+              }}
+            />
+          </div>
+          <div className="addSales-form-input">
+            <label htmlFor="Sale">Sale Price</label>
+            <input
+              type="text"
+              placeholder="Sale Price"
+              id="Sale"
+              onChange={(e) => {
+                setSale(e.target.value);
+              }}
+            />
+          </div>
         </div>
-        <hr />
-        <br />
-        <table>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Image</th>
-                    <th>Sale Price</th>
-                    <th>Action</th>
-                    </tr>
-            </thead>
-            <tbody>
-                {salesProducts.map((sales, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{sales.Title}</td>
-                            <td>{sales.Description}</td>
-                            <td>{sales.price}</td>
-                            <td>{sales.imgLink}</td>
-                            <td>{sales.sale}</td>
-                            <td>
-                                <button onClick={() => {
-                                    setShowSale(false)
-                                }}>Edit</button>
-                                <button onClick={() => {
-                                    setShowSale(false)
-                                }}>Delete</button>
-                            </td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-
-
-        </table>
+        <div className="addSales-form-button">
+          <button
+            onClick={async () => {
+              try {
+                const docRef = await addDoc(collection(db, "sales"), {
+                  Title: title,
+                  Description: description,
+                  price: price,
+                  imgLink: imgLink,
+                  sale: sale,
+                });
+                console.log("Document written with ID: ", docRef.id);
+              } catch (e) {
+                console.error("Error adding document: ", e);
+              }
+              salesUpdateData();
+              setTitle("");
+              setDescription("");
+              setPrice("");
+              setImgLink("");
+              setSale("");
+            }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+      <hr />
+      <br />
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Image</th>
+            <th>Sale Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {salesProducts.map((sales, index) => {
+            return (
+              <tr key={index}>
+                <td>{sales.Title}</td>
+                <td>{sales.Description}</td>
+                <td>{sales.price}</td>
+                <td>{sales.imgLink}</td>
+                <td>{sales.sale}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      setShowSale(false);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => {
+                      setShowSale(false);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-    
-  )
+  );
 }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,59 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { db } from "../../config/Config";
 import "./Users.css";
 
-import {collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
 export default function Users() {
-    const [users, setUsers] = useState([])
-    const getUsers = async() => {
-        let allUsers=[]
-          const querySnapshot = await getDocs(collection(db, "user"));
-          querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            let data = {...doc.data(), id: doc.id};
-            allUsers.push(data);
-          });
-          setUsers(allUsers)
-          console.log("user data ",users)
-        }
-    useEffect(() => {
-        getUsers()
-    }, [])
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    let allUsers = [];
+    const querySnapshot = await getDocs(collection(db, "user"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      let data = { ...doc.data(), id: doc.id };
+      allUsers.push(data);
+    });
+    setUsers(allUsers);
+    console.log("user data ", users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <div>
-        <h1>Users</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Zip</th>
-                    <th>Country</th>
-                    </tr>
-            </thead>
-            <tbody>
-                {users.map((user, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.phone}</td>
-                            <td>{user.city}</td>
-                            <td>{user.state}</td>
-                            <td>{user.zip}</td>
-                            <td>{user.country}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>    
-        </table>
-
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.address}</td>
+                <td>{user.city}</td>
+                <td>{user.state}</td>
+                <td>{user.zip}</td>
+                <td>{user.country}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
